de79af7a74f1f47208810bebfd4dbbeb1ace0482
sceva <sy@foreignlanguageflashcards.com>
2014-02-22 17:46:16 -0800

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -23,21 +23,42 @@ class HomePageTest(TestCase):
         # self.assertIn(b'<title>To-Do lists</title>', response.content)
         # self.assertTrue(response.content.endswith(b'</html>'))
 
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
     def test_home_page_can_save_a_POST_request(self):
-        # Setup
+        # 1. Setup
         request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = "A new list item"
-        # Exercise
+        # 2. Exercise
         response = home_page(request)
-        # Assert
-        self.assertIn('A new list item', response.content.decode())
+        # 3. Assert
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
 
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': 'A new list item'}
-        )
-        self.assertEqual(response.content.decode(), expected_html)
+    def test_home_page_redirects_after_a_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = "A new list item"
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_only_saves_item_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
 

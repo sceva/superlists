771e099ec42d110e70dd084c8ad2bc781c2a83e1
sceva <sy@foreignlanguageflashcards.com>
2014-03-04 07:18:22 -0800

    Refactor list view to handle new item POSTs

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -12,7 +12,8 @@ class ItemValidationTest(FunctionalTest):
         # The ome page refreshes, and there is an error message saying
         # that list items cannot be blank
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.txt, "You can't have an empty list item")
+        self.assertEqual(error.text, "You can't have an empty list item")
+
 
         # She tries again with some text for the item, which now works
         self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
@@ -24,7 +25,7 @@ class ItemValidationTest(FunctionalTest):
         # She receives a similar warning on the list page
         self.check_for_row_in_list_table('1: Buy milk')
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.txt, "You can't have an empty list item")
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # And she can correct it by filling some text in
         self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')

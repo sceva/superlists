8b92e06b3202eea071a38f311e737c60f86dad9d
sceva <sy@foreignlanguageflashcards.com>
2014-03-12 19:03:17 -0500

    Mocked persona in javascript tests, got login/out working, end of chapter 15

---
+++ accounts/static/accounts.js
@@ -0,0 +1,28 @@
+/**
+ * Created by s on 3/12/14.
+ */
+
+var initialize = function (navigator, user, token, urls) {
+    $('#id_login').on('click', function () {
+        navigator.id.request();
+    });
+
+    navigator.id.watch({
+        loggedInUser: user,
+        onlogin: function (assertion) {
+            $.post(
+                urls.login,
+                {assertion: assertion, csrfmiddlewaretoken: token }
+            )
+                .done(function () { window.location.reload(); })
+                .fail(function () { navigator.id.logout(); });
+        },
+        onlogout: function () {}
+    });
+};
+
+window.Superlists = {
+    Accounts: {
+        initialize: initialize
+    }
+};
